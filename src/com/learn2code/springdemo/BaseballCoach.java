8f2b8a031f16a9e96f9ba784b1fbfd41e3b999a9
adna_coder <mujibur.rehman@citiustech.com>
2019-07-25 20:26:09 +0530

    DI constructor based DI implemented

--- src/com/learn2code/springdemo/BaseballCoach.java
+++ src/com/learn2code/springdemo/BaseballCoach.java
@@ -2,6 +2,12 @@ package com.learn2code.springdemo;
 
 public class BaseballCoach implements Coach {
 
+    private FortuneService fortuneService;
+
+    public BaseballCoach(FortuneService fs) {
+        fortuneService = fs;
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Spend 30 minutes on batting practice.";
