f7376feb279576c5da77f38cc654dd6c9fbe2ebd
adna_coder <mujibur.rehman@citiustech.com>
2019-08-08 20:34:52 +0530

    L-45: singletone bean-scope implemented

--- src/com/learn2code/beanscope/beanScopeApp.java
+++ src/com/learn2code/beanscope/beanScopeApp.java
@@ -8,8 +8,18 @@ public class beanScopeApp {
 
         Gamer videoGamer = context.getBean("videoGamerBean", Gamer.class);
 
+        Gamer anotherVideoGamer = context.getBean("videoGamerBean", Gamer.class);
+
         System.out.println(videoGamer.subscribeToGame());
         System.out.println(videoGamer.printGamingCommunity());
+
+        // --- Checking if both beans are same i.e their scope is singletone
+        boolean result = (videoGamer == anotherVideoGamer);
+
+        System.out.println("\n Pointing to same object :: " + result);
+        System.out.println("\n Memory location for videoGamer :: " + videoGamer);
+        System.out.println("\n Memory location for anotherVideoGamer :: " + anotherVideoGamer);
+
         context.close();
     }
 }
